78b75b5b291cbd342955dd62c1df01f11103dc3b
brian9952 <fbriann@gmail.com>
2022-08-01 00:08:28 +0700

    frontend change

---
+++ frontend/crud/src/views/ProductManagement.vue
@@ -0,0 +1,22 @@
+<script setup>
+import "../../node_modules/primeflex/primeflex.css"
+
+</script>
+
+<template>
+
+<div class="card">
+    <h5>Vertical and Grid</h5>
+    <div class="formgrid grid">
+        <div class="field col">
+            <label for="firstname2">Firstname</label>
+            <input id="firstname2" type="text" class="text-base text-color surface-overlay p-2 border-1 border-solid surface-border border-round appearance-none outline-none focus:border-primary w-full">
+        </div>
+        <div class="field col">
+            <label for="lastname2">Lastname</label>
+            <input id="lastname2" type="text" class="text-base text-color surface-overlay p-2 border-1 border-solid surface-border border-round appearance-none outline-none focus:border-primary w-full">
+        </div>
+    </div>
+</div>
+
+</template>
